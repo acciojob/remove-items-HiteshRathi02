ae86ea2c23f819b0200ab0d52fdd75c271057093
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 18:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const color = document.getElementById("colorSelect");
+  color.removeChild(color.options[color.selectedIndex]);
+
+  
+});
